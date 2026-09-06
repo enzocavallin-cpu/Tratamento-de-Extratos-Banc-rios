ff39d1680c7f44415751529383c9ad05f814e6e4
enzocavallin-cpu <enzo.cavallin@gmail.com>
2026-09-06 15:13:07 -0300

    Update Site de Extratos.py

--- Site de Extratos.py
+++ Site de Extratos.py
@@ -1104,7 +1104,7 @@ def ce_cp(output_pdf):
     savings_account['Descrição'] = savings_account['Descrição'].str.replace('Rendimento', 'Saldo Final')
     
     condition = savings_account['Descrição'] == 'Saldo Final'
-    savings_account['Data'] = savings_account['Data'].mask(condition, savings_account['Data'] + pd.Timedelta(days=30))
+    savings_account['Data'] = savings_account['Data'].mask(condition, savings_account['Data'] + pd.Timedelta(days=29))
 
     return savings_account
 
